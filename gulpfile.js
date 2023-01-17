7d9fda1217a38dbbe65faf811bc5c653fb9597e0
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2023-01-16 23:12:59 -0500

    Creando imágenes Avif
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const plumber = require( 'gulp-plumber' );
 const cache = require( 'gulp-cache' );
 const imagemin = require( 'gulp-imagemin' );
 const webp = require( 'gulp-webp' );
-// const avif = require( 'gulp-avif' );
+const avif = require( 'gulp-avif' );
 
 /**
  * @name css-loader
@@ -62,16 +62,16 @@ function versionWebp ( done ) {
  * @description Cambiar las imágenes a formato avif
  * @param {*} done 
  */
-// function versionAvif ( done ) {
-//   const opciones = {
-//     quality: 50,
-//   };
-//   src( 'src/img/**/*.{png,jpg}' )
-//     .pipe( avif( opciones ) )
-//     .pipe( dest( 'build/img' ) );
+function versionAvif ( done ) {
+  const opciones = {
+    quality: 50,
+  };
+  src( 'src/img/**/*.{png,jpg}' )
+    .pipe( avif( opciones ) )
+    .pipe( dest( 'build/img' ) );
   
-//   done();
-// }
+  done();
+}
 
 /**
  * @name dev-watch
@@ -87,5 +87,5 @@ function dev (done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-// exports.versionAvif = versionAvif;
-exports.dev = parallel( imagenes, versionWebp, dev );
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev );
