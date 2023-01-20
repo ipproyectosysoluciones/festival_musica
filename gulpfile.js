dda2ed62a3be6c42cdde07b89e251b4f35acb036
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2023-01-19 21:28:06 -0500

    Mejorando el Código con Gulp y CSS Nano, Sourcemaps, Terser
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require( 'gulp' );
 /* CSS */
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
 const plumber = require( 'gulp-plumber' );
+const autoprefixer = require( 'autoprefixer' );
+const cssnano = require( 'cssnano' );
+const postcss = require( 'gulp-postcss' );
+const sourcemaps = require( 'gulp-sourcemaps' );
 
 /* Imágenes */
 const cache = require( 'gulp-cache' );
@@ -11,17 +15,20 @@ const webp = require( 'gulp-webp' );
 const avif = require( 'gulp-avif' );
 
 /* Javascript */
-// const terser = require( 'gulp-terser-js' );
+const terser = require( 'gulp-terser-js' );
 
 /**
  * @name css-loader
  * @description Identificar, compilar y almacenar todos los archivos SCSS
  * @param {*} done
  */
-function css (done) {
+function css ( done ) {
   src( 'src/scss/**/*.scss' ) // Identificar el archivo SASS
+    .pipe( sourcemaps.init() )
     .pipe( plumber() )
     .pipe( sass() ) // Compilarlo
+    .pipe( postcss( [ autoprefixer(), cssnano(), ] ) )
+    .pipe( sourcemaps.write( '.' ) )
     .pipe( dest( 'build/css' ) ); // Almacenarlo en el HardDisk
 
   done(); // Callback que avisa a gulp cuanta la functio finaliza
@@ -78,11 +85,14 @@ function versionAvif ( done ) {
 
 /**
  * @name javascript
- * @description 
+ * @description Compilación de javascript
  * @param {*} done 
  */
 function javascript( done ) {
   src( 'src/js/**/*.js' )
+    .pipe( sourcemaps.init() )
+    .pipe( terser() )
+    .pipe( sourcemaps.write() )
     .pipe( dest( 'build/js' ) );
 
   done();
